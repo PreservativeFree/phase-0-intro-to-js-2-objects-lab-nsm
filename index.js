6ae0707b30de008be758dfeefa07f377201542df
Nelson Moreno <thegreyabove@gmail.com>
2024-12-02 21:04:10 -0500

    Object lab commit

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {
+    name: "Eli",
+    streetAddress: "456 Ocean Street",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const employee1 = {...obj}
+
+    employee1[key] = value
+
+    return employee1
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const employee1 = {...obj}
+
+    delete employee1[key]
+
+    return employee1
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key]
+    
+    return obj
+}
